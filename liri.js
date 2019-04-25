349e3118bdc3fbd65923f1f285924f45c55eca34
DavidmB <david.m.bedford1@gmail.com>
2019-04-25 17:20:56 -0400

    omdb improved - it now shows all info. items added fro spotify search functions

--- liri.js
+++ liri.js
@@ -1,21 +1,17 @@
-//var dotenv = require("dotenv").config()
-
-//var keys = require("./keys.js");
+const dotenv = require("dotenv").config();
+const axios = require("axios");
+const spotify = require('node-spotify-api');
+let keys = require("./keys.js");
 
 //var spotify = new Spotify(keys.spotify);
 // Client ID 5dd7210c7e764cf483dc8add6dab1d61
 // Client Secret 12c754344f154dfca62a171195cff7ce
 
-//var bandsitUrl = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp"
-///////////////////////////////////////////////////////////////////////////////////////////////////
-var axios = require("axios");
+let nodeArgs = process.argv;
 
-var nodeArgs = process.argv;
+let inputName = "";
 
-var inputName = "";
-
-for (var i = 2; i < nodeArgs.length; i++) {
-
+for (let i = 2; i < nodeArgs.length; i++) {
   if (i > 2 && i < nodeArgs.length) {
     inputName = inputName + "+" + nodeArgs[i];
   }
@@ -41,6 +37,9 @@ for (var i = 2; i < nodeArgs.length; i++) {
 // The album that the song is from            /
 //////////////////////////////////////////////
 
+
+
+
 // movie-this
 // node liri.js movie-this '<movie name here>'
 //////////////////////////////////////////
@@ -54,13 +53,20 @@ for (var i = 2; i < nodeArgs.length; i++) {
 // * Actors in the movie.                 /
 //////////////////////////////////////////
 
-var movQueryUrl = "http://www.omdbapi.com/?t=" + inputName + "&y=&plot=short&apikey=trilogy";
-
-console.log(movQueryUrl);
+let movQueryUrl = "http://www.omdbapi.com/?t=" + inputName + "&y=&plot=short&apikey=trilogy";
 
 axios.get(movQueryUrl).then(
   function(response) {
+    console.log(movQueryUrl);
+    console.log("\n===================\n")
+    console.log("Movie Title: " + response.data.Title);
     console.log("Release Year: " + response.data.Year);
+    console.log("IMDB Rating: " + response.data.Ratings[0].Value);
+    console.log("Rotten Tomatoes Rating: " + response.data.Ratings[1].Value );
+    console.log("This movie was produced in (country): " + response.data.Country );
+    console.log("Language: " + response.data.Language);
+    console.log("Plot: " + response.data.Plot);
+    console.log("Actors: " + response.data.Actors);
   }
 );
 
